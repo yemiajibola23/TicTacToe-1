cc593b7c704e7463a0853bb7be381d0386422f1a
Michael Berger <michaeledwardberger@live.com>
2016-01-14 15:35:31 -0600

    Added two methods

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -30,9 +30,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.player = self.whichPlayerLabel.text;
-    self.grid = @[self.buttonOne,self.buttonTwo,self.buttonThree,
-                        self.buttonFour, self.buttonFive, self.buttonSix,
-                        self.buttonSeven, self.buttonEight, self.buttonNine];
+    self.grid = @[self.buttonOne,   self.buttonTwo,   self.buttonThree,
+                  self.buttonFour,  self.buttonFive,  self.buttonSix,
+                  self.buttonSeven, self.buttonEight, self.buttonNine];
+    
+    
 }
 
 - (IBAction)onButtonTapped:(UIButton *)sender {
@@ -50,6 +52,25 @@
 
     }
     self.whichPlayerLabel.text = self.player;
+}
+
+-(void)proclaimVictory:(NSString *)winner {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"WINNER!"
+                                                                   message:[NSString
+                                                                            stringWithFormat:@"%@ has won!",winner]
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Play Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self resetGame];
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+    
+
+}
+
+-(void)resetGame {
+    //Reset game logic
 }
 
 @end

4e48037ac00245900cfc1bd658fe2ef83b67e0d5
Jon Jones <jon.p.jones13@gmail.com>
2016-01-14 14:46:38 -0600

    Started the project.

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,19 +9,41 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *buttonOne;
+@property (weak, nonatomic) IBOutlet UIButton *buttonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFour;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFive;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSix;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSeven;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEight;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
+@property NSString *player;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.player = self.whichPlayerLabel.text;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onButtonTapped:(UIButton *)sender {
+    [sender setTitle:self.player forState:UIControlStateNormal];
+    
+    
+    if ([self.player isEqualToString:@"X"]) {
+        self.player = @"O";
+        [sender setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    } else {
+        self.player = @"X";
+        [sender setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+
+    }
+    self.whichPlayerLabel.text = self.player;
 }
 
 @end
